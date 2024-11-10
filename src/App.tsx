a8425e97629cb67e1bc3b06cd47328a3e32ffa00
AnsarKhubiev <anskhubiev@icloud.com>
2024-11-10 23:58:53 +0300

    sprint 02-01 - hw completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
-import { HW1 } from './HW/1/HW1';
-import { HW2 } from './HW/2/HW2';
-import { HW3 } from './HW/3/HW3';
-import { HW4 } from './HW/4/HW4';
+import { HW1 } from './HW/sprint_01/1/HW1';
+import { HW2 } from './HW/sprint_01/2/HW2';
+import { HW3 } from './HW/sprint_01/3/HW3';
+import { HW4 } from './HW/sprint_01/4/HW4';
+import {HwSprint2Week1} from "./HW/sprint_02/01/HwSprint2Week1";
+
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       {/*<HW1 />*/}
       {/*<HW2 />*/}
       {/*<HW3 />*/}
-      <HW4 />
+      {/*<HW4 />*/}
+      <HwSprint2Week1/>
     </div>
   );
 }
